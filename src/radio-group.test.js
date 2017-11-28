e0508b1c2e274b7da0fe54abdd62c00f9b55a9c6
thebearingedge <contact@timdav.is>
2017-11-27 19:44:23 -0800

    Migrate RadioGroup implementation. #2

---
+++ src/radio-group.test.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { describe, it } from 'mocha'
+import { expect, mountWith, mockField, spy, stub } from './__test__'
+import RadioGroup from './radio-group'
+import Radio from './radio'
+
+describe('RadioGroup', () => {
+
+  const context = { registerField: mockField }
+  const mount = mountWith({ context })
+
+  it('checks its matching radio input', () => {
+    const wrapper = mount(
+      <RadioGroup name='test' value='bar'>
+        <Radio value='foo'/>
+        <Radio value='bar'/>
+        <Radio value='baz'/>
+      </RadioGroup>
+    )
+    expect(wrapper.find('input[value="foo"]')).not.to.be.checked()
+    expect(wrapper.find('input[value="bar"]')).to.be.checked()
+    expect(wrapper.find('input[value="baz"]')).not.to.be.checked()
+  })
+
+  it('updates its field when its radio inputs change', () => {
+    const wrapper = mount(
+      <RadioGroup name='test'>
+        <Radio value='foo'/>
+        <Radio value='bar'/>
+        <Radio value='baz'/>
+      </RadioGroup>
+    )
+    const update = spy(wrapper.instance().field, 'update')
+    wrapper.find('input[value="baz"]').simulate('change')
+    expect(update).to.have.been.calledWith({
+      value: 'baz',
+      isTouched: true
+    })
+  })
+
+  it('updates its field after its radio inputs blur', done => {
+    const wrapper = mount(
+      <RadioGroup name='test'>
+        <Radio value='foo'/>
+        <Radio value='bar'/>
+        <Radio value='baz'/>
+      </RadioGroup>
+    )
+    stub(wrapper.instance().field, 'update')
+      .callsFake(state => {
+        expect(state).to.deep.equal({ isTouched: true })
+        done()
+      })
+    wrapper.find('input[value="foo"]').simulate('blur')
+  })
+
+})
